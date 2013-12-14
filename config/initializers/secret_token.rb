873e43abb90f57ecd92103d2aa001ca641795210
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2013-12-14 23:03:09 +0100

    secret token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Memoreal::Application.config.secret_token = 'bc976abf72d939f0e777aab2dfab0085ef1883aa3d268f0308c06e20e062d5d60b985ab21bfaa53f7d5dff1a07d1231358f5ed252cdb56044b9436fb2372dee7'
