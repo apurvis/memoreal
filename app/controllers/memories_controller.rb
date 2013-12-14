3c1b6cca08048f031c2a22bece57a686a4c4ab86
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2013-12-14 17:20:01 +0100

    Show memories

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -3,6 +3,19 @@ class MemoriesController < ApplicationController
   end
 
   def create
-    render text: params[:memory].inspect
+    @memory = Memory.new(params[:memory])
+    @memory.save
+
+    redirect_to @memory
+  end
+
+  def show
+    @memory = Memory.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:memory).permit(:description)
   end
 end
--- app/models/memory.rb
+++ app/models/memory.rb
@@ -1,4 +1,6 @@
 class Memory < ActiveRecord::Base
   has_one :previous_memory, :class_name => "Memory", :foreign_key => "previous_memory_id"
   has_one :visitor
+
+  attr_accessible :description
 end
